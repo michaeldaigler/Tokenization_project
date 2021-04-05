b7a99c27f0971860fa492d83403c245c3e33277b
Michael Daigler <mdaigler2601@gmail.com>
2021-04-05 17:12:37 -0400

    adding mroe tests

--- contracts/DToken.sol
+++ contracts/DToken.sol
@@ -1,9 +1,7 @@
-pragma solidity ^0.8.0;
-
+pragma solidity >=0.6.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
 contract DToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("DToken", "DDD") {
-        _mint(msg.sender, initialSupply);
-    }
+constructor(uint256 initialSupply) ERC20("DToken", "DDD") public {
+_mint(msg.sender, initialSupply); _setupDecimals(0);
+}
 }
--- test/DToken.test.js
+++ test/DToken.test.js
@@ -3,23 +3,47 @@ const DToken = artifacts.require("DToken.sol");
 
 var chai = require("chai");
 const BN = web3.utils.BN;
-const chaiBN = require("chai-bn")(BN);
 
+const chaiBN = require("chai-bn")(BN);
 chai.use(chaiBN);
 
 var chaiAsPromised = require("chai-as-promised");
-
 chai.use(chaiAsPromised);
-
 const expect = chai.expect;
 
 contract("Token test", async (accounts) => {
+
+    const [deployerAccount, recipientAccount, anotherAccount] = accounts;
+
     it("all tokens should be in my account", async () => {
         let instance = await DToken.deployed();
         let totalSupply = await instance.totalSupply();
-        let balance = await instance.balanceOf(accounts[0]);
-        console.log(totalSupply)
-        // assert.equal(balance.valueOf(), initialSupply.valueOf(), "The balance was not the same");
-        expect(instance.balanceOf(accounts[0])).to.eventually.be.a.bignumber.equal(totalSupply);
+
+        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply);
     })
+
+    it("is possible to send tokens between accounts", async () => {
+        const amountTokensSent = 1;
+        let instance = await DToken.deployed();
+        let totalSupply = await instance.totalSupply();
+        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply);
+        expect(instance.transfer(recipientAccount, amountTokensSent)).to.be.eventually.fulfilled;
+        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equals(totalSupply.sub(new BN(amountTokensSent)));
+        expect(instance.balanceOf(recipientAccount)).to.eventually.be.a.bignumber.equal(new BN(amountTokensSent));
+    })
+
+    before(() => {
+
+    })
+
+    it("is not possible to send more tokens than is available", async () => {
+        let instance = await DToken.deployed();
+        let balanceOfAccount = await instance.balanceOf(deployerAccount);
+
+        expect(instance.transfer(recipientAccount, (balanceOfAccount + 1))).to.be.rejected;
+
+        expect(instance.balanceOf(balanceOfAccount)).to.eventually.be.a.bignumber.equal(balanceOfAccount)
+
+   })
+
 })
